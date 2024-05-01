3acc98db112bac1ecedcaee99e3062302fa731f4
Marwan1 <mtm.1234elmasry@gmail.com>
2024-05-01 20:21:49 +0300

    new cert

--- src/certificate.jsx
+++ src/certificate.jsx
@@ -123,6 +123,18 @@ class Certifcate extends Component {
                             <p>UX design, short for User Experience design, is the process of designing and enhancing the overall experience that users have when interacting with a product, system, or service. It focuses on meeting the needs and expectations of users by creating intuitive, efficient, and enjoyable experiences.</p>
                         </div>
                     </div>
+                    <div className='card'>
+                    <div className='info'>
+                            <h3>PHP & Mysql</h3>
+                            {/* <p>UX design, short for User Experience design, is the process of designing and enhancing the overall experience that users have when interacting with a product, system, or service. It focuses on meeting the needs and expectations of users by creating intuitive, efficient, and enjoyable experiences.</p> */}
+                        </div>
+                        <div className='box'>
+                            <a href="https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=36489&code=xPIULpR0Vq&qrcode=1">
+                            <img src='./img/php.png'  alt='php' />
+                            </a>
+                        </div>
+                        
+                    </div>
                 </div>
                 <div className="link">
                     <h2>For More Info <a className="in" href="https://www.linkedin.com/in/marwan-tarek-elmasry-5a46b5275/">LinkedIN</a></h2>
