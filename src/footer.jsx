73a8a03d30432b2a2d42c20c358165ef4a130c10
Marwan1 <mtm.1234elmasry@gmail.com>
2024-05-02 18:00:01 +0300

    foot

--- src/footer.jsx
+++ src/footer.jsx
@@ -7,10 +7,10 @@ class Footer extends Component {
     render() { 
         return ( <div className="body">
                         <div className="foot">
-                <div className="container">
-                    
-                </div>
-            </div>
+                            <div className="container">
+                            <p>&copy; This Website has All Rights Reserved </p>
+                            </div>
+                        </div>
         </div> );
     }
 }
