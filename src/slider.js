7d6c152348ce779a10f93529ed88abcc21021952
Marwan1 <mtm.1234elmasry@gmail.com>
2024-06-27 01:44:31 +0300

    v2 of Projects

--- src/slider.js
+++ src/slider.js
@@ -17,42 +17,42 @@ export default function SimpleSlider() {
       <div>
       <h2>Level 1</h2>
       <h3>project 1</h3>
-      <a href="https://react-slick.neostack.com/docs/example/simple-slider">
+      <a href="https://marwan-elmasrry.github.io/myproject/index1.html">
       <img src="./img/project1.png" />
       </a>
       </div>
       <div>
       <h2>Level 1</h2>
       <h3>project 2</h3>
-      <a href="https://react-slick.neostack.com/docs/example/simple-slider">
+      <a href="https://marwan-elmasrry.github.io/code-soft/level-1/task-1/inde.html">
       <img src="./img/project2.png" />
       </a>
       </div>
       <div>
       <h2>Level 1</h2>
       <h3>project 3</h3>
-      <a href="https://react-slick.neostack.com/docs/example/simple-slider">
+      <a href="https://marwan-elmasrry.github.io/code-soft/level-1/task-2/index.html">
       <img src="./img/project3.png" />
       </a>
       </div>
       <div>
       <h2>Level 1</h2>
       <h3>project 4</h3>
-      <a href="https://react-slick.neostack.com/docs/example/simple-slider">
+      <a href="https://marwan-elmasrry.github.io/Code-Alpha/task1/task%201/index.html">
       <img src="./img/project4.png" />
       </a>
       </div>
       <div>
       <h2>Level 1</h2>
       <h3>project 5</h3>
-      <a href="https://react-slick.neostack.com/docs/example/simple-slider">
+      <a href="https://marwan-elmasrry.github.io/Code-Alpha/task1/task%202/index.html">
       <img src="./img/project5.png" />
       </a>
       </div>
       <div>
       <h2>Level 1</h2>
       <h3>project 6</h3>
-      <a href="https://react-slick.neostack.com/docs/example/simple-slider">
+      <a href="https://marwan-elmasrry.github.io/code-soft/level-1/task-3/index.html">
       <img src="./img/project6.png" />
       </a>
       </div>
