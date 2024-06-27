50eccdc4885d1f9a2da12f47029e066c4c86bb6f
Marwan1 <mtm.1234elmasry@gmail.com>
2024-06-27 23:38:15 +0300

    level 2 complete

--- src/slider2.js
+++ src/slider2.js
@@ -28,6 +28,34 @@ export default function SimpleSlider2() {
       <img src="./img/project8.png" />
       </a>
       </div>
+      <div>
+      <h2>Level 2</h2>
+      <h3>project 3</h3>
+      <a href="https://marwan-elmasrry.github.io/Shopping-Cart/">
+      <img src="./img/project9.png" />
+      </a>
+      </div>
+      <div>
+      <h2>Level 2</h2>
+      <h3>project 4</h3>
+      <a href="https://marwan-elmasrry.github.io/Todo-List-by-react/">
+      <img src="./img/project10.png" />
+      </a>
+      </div>
+      <div>
+      <h2>Level 2</h2>
+      <h3>project 5</h3>
+      <a href="https://marwan-elmasrry.github.io/Budjet/">
+      <img src="./img/project11.png" />
+      </a>
+      </div>
+      <div>
+      <h2>Level 2</h2>
+      <h3>project 6</h3>
+      <a href="https://marwan-elmasrry.github.io/Expense-Tracker/">
+      <img src="./img/project12.png" />
+      </a>
+      </div>
     </Slider>
     
   );
