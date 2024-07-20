048de35e0abbab2c3878b2ff2a8593c7da1e5340
Marwan1 <mtm.1234elmasry@gmail.com>
2024-07-20 23:02:14 +0300

    array in state and render it

--- src/certificate.jsx
+++ src/certificate.jsx
@@ -3,147 +3,88 @@ import React, { Component } from 'react';
 import './home.css';
 
 class Certifcate extends Component {
-    state = {  } 
+    state = { 
+        card:[
+            {id:1
+                ,link:"https://www.coursera.org/account/accomplishments/verify/FEDXW2FMK4GY"
+                ,img:'./img/ibm1.png'
+                ,altI:'Introduction to Software Engineering'
+                ,h3:"Introduction to Software Engineering"},
+            {id:2
+                ,link:"https://www.coursera.org/account/accomplishments/verify/G42T55M3H99W"
+                ,img:'./img/ibm2.png'
+                ,altI:'Web Development with HTML, CSS, JavaScript'
+                ,h3:"Web Development with HTML, CSS, JavaScript"},
+            {id:3
+                ,link:"https://www.coursera.org/account/accomplishments/verify/F9F852EEYSFJ"
+                ,img:'./img/ibm3.png'
+                ,altI:'Git and GitHub'
+                ,h3:"Git and GitHub"},
+            {id:4
+                ,link:"https://www.coursera.org/account/accomplishments/verify/UQCGYDD6Y3YZ"
+                ,img:'./img/ibm4.png'
+                ,altI:'React.js'
+                ,h3:"React.js"},
+            {id:5
+                ,link:"https://www.udemy.com/certificate/UC-14f4ca4a-e5f8-4702-ba1c-708f8c8c3927/"
+                ,img:'./img/php-u.png'
+                ,altI:'PHP'
+                ,h3:"PHP"},
+            {id:6
+                ,link:"https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=35649&code=MmhggJBJeV&qrcode=1"
+                ,img:'./img/first certificate.png'
+                ,altI:'Html and Css'
+                ,h3:"Html and Css"},
+            {id:7
+                ,link:"https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=162768&code=c0AVqxitaT&qrcode=1"
+                ,img:'./img/socend certificate.png'
+                ,altI:'JavaScript'
+                ,h3:"JavaScript"},
+            {id:8
+                ,link:"https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=206156&code=25vGB7epqc&qrcode=1"
+                ,img:'./img/ES 6.png'
+                ,altI:'Modern JavaScript (ES6)'
+                ,h3:"Modern JavaScript (ES6)"},
+            {id:9
+                ,link:"https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=194868&code=mb5Y8FRIK6&qrcode=1"
+                ,img:'./img/fourth certificate.png'
+                ,altI:'React Js'
+                ,h3:"React Js"},
+            {id:10
+                ,link:"https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=66871&code=Doqa4cTMAB&qrcode=1"
+                ,img:'./img/theerd certificate.png'
+                ,altI:'python'
+                ,h3:"python"},
+            {id:11
+                ,link:"https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=282311&code=PgwMu0Tszf&qrcode=1"
+                ,img:'./img/certx.png'
+                ,altI:'UX Design'
+                ,h3:"UX Design"},
+            {id:12
+                ,link:"https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=36489&code=xPIULpR0Vq&qrcode=1"
+                ,img:'./img/php.png'
+                ,altI:'php'
+                ,h3:"PHP & Mysql"},
+        ]
+    } 
     render() { 
         return ( <div className="body">
                         <div className="cert">
-            <div className='container'>
+                <div className='container'>
                     <h1>Certifcate</h1>
                 <div className='cert-cont'>
-                    <div className='card'>
+                    {this.state.card.map(card=>(
+                        <div className='card' key={card.id}>
                         <div className='box'>
-                            <a href="https://www.coursera.org/account/accomplishments/verify/FEDXW2FMK4GY"> 
-                            <img src='./img/ibm1.png'  alt='Introduction to Software Engineering' />
+                            <a href={card.link}> 
+                            <img src={card.img}  alt={card.altI} />
                             </a>
                         </div>
                         <div className='info'>
-                            <h3>Introduction to Software Engineering</h3>
-                            {/* <p>Software engineering is a discipline that involves the systematic and disciplined approach to designing, developing, testing, and maintaining software systems. It encompasses a set of principles, methods, and tools that are used to create high-quality software solutions.</p> */}
+                            <h3>{card.h3}</h3>
                         </div>
                     </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://www.coursera.org/account/accomplishments/verify/G42T55M3H99W">
-                            <img src='./img/ibm2.png'  alt='Web Development with HTML, CSS, JavaScrip' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3> Web Development with HTML, CSS, JavaScript</h3>
-                            {/* <p>HTML (Hypertext Markup Language): HTML is the standard markup language used to structure the content of web pages.</p> */}
-                            {/* <p>CSS (Cascading Style Sheets): CSS is used to control the presentation and appearance of web pages.</p> */}
-                            {/* <p>JavaScript: JavaScript is a programming language that enables dynamic and interactive behavior on web pages.</p> */}
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://www.coursera.org/account/accomplishments/verify/F9F852EEYSFJ">
-                            <img src='./img/ibm3.png'  alt='Git and GitHub' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3> Git and GitHub</h3>
-                            {/* <p>Git is a distributed version control system (VCS) that is commonly used for tracking changes in source code during software development.</p> */}
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://www.coursera.org/account/accomplishments/verify/UQCGYDD6Y3YZ">
-                            <img src='./img/ibm4.png'  alt='Git and GitHub' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3>React.js</h3>
-                            {/* <p>React.js, commonly referred to as React, is an open-source JavaScript library for building user interfaces (UIs) or user interface components.  </p> */}
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://www.udemy.com/certificate/UC-14f4ca4a-e5f8-4702-ba1c-708f8c8c3927/">
-                            <img src='./img/php-u.png'  alt='PHP' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3>PHP</h3>
-                        </div>
-                    </div>
-                    <div className='card'>  
-                        <div className='box'>
-                            <a href="https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=35649&code=MmhggJBJeV&qrcode=1">
-                            <img src='./img/first certificate.png'  alt='Html and Css' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3>Html and Css</h3>
-                            {/* <p>HTML (Hypertext Markup Language): HTML is the standard markup language used to structure the content of web pages.</p> */}
-                            {/* <p>CSS (Cascading Style Sheets): CSS is used to control the presentation and appearance of web pages.</p> */}
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=162768&code=c0AVqxitaT&qrcode=1">
-                            <img src='./img/socend certificate.png'  alt='JavaScript' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3>JavaScript</h3>
-                            {/* <p>JavaScript: JavaScript is a programming language that enables dynamic and interactive behavior on web pages.</p> */}
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=206156&code=25vGB7epqc&qrcode=1">
-                            <img src='./img/ES 6.png'  alt='Modern JavaScript (ES6)' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3>Modern JavaScript (ES6) </h3>
-                            {/* <p>Modern JavaScript, also known as ECMAScript 6 (ES6) or ES2015, refers to the latest version of the JavaScript language specification. It introduced significant enhancements and new features to JavaScript, making it more powerful, expressive, and easier to work with. ES6 was released in 2015 and has since become widely adopted by web developers.</p> */}
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=194868&code=mb5Y8FRIK6&qrcode=1">
-                            <img src='./img/fourth certificate.png'  alt='React Js' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3>React Js</h3>
-                            {/* <p>React.js, commonly referred to as React, is an open-source JavaScript library used for building user interfaces for web applications. It was developed by Facebook and is currently maintained by Facebook and a community of individual contributors.</p> */}
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=66871&code=Doqa4cTMAB&qrcode=1">
-                            <img src='./img/theerd certificate.png'  alt='python' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3>python</h3>
-                            {/* <p>Python is a high-level, interpreted programming language known for its simplicity, readability, and versatility. It was created by Guido van Rossum and first released in 1991. Python emphasizes code readability and a clean syntax, which makes it easy to learn and understand.</p> */}
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=282311&code=PgwMu0Tszf&qrcode=1">
-                            <img src='./img/certx.png'  alt='UX Design' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3>UX Design</h3>
-                            {/* <p>UX design, short for User Experience design, is the process of designing and enhancing the overall experience that users have when interacting with a product, system, or service. It focuses on meeting the needs and expectations of users by creating intuitive, efficient, and enjoyable experiences.</p> */}
-                        </div>
-                    </div>
-                    <div className='card'>
-                        <div className='box'>
-                            <a href="https://maharatech.gov.eg/mod/customcert/verify_certificate.php?contextid=36489&code=xPIULpR0Vq&qrcode=1">
-                            <img src='./img/php.png'  alt='php' />
-                            </a>
-                        </div>
-                        <div className='info'>
-                            <h3>PHP & Mysql</h3>
-                            {/* <p>UX design, short for User Experience design, is the process of designing and enhancing the overall experience that users have when interacting with a product, system, or service. It focuses on meeting the needs and expectations of users by creating intuitive, efficient, and enjoyable experiences.</p> */}
-                        </div>
-                    </div>
+                    ))}
                 </div>
                 <div className="link">
                     <h2>For More Info <a className="in" href="https://www.linkedin.com/in/marwan-tarek-elmasry-5a46b5275/">LinkedIN</a></h2>
