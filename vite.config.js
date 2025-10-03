af83c8043a777fe0879bd53b2b1f80341fda4073
MARWAN-elmasrry <mtm.1234elmasry@gmail.com>
2025-10-03 16:49:51 +0300

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: process.env.VITE_BASE_PATH || "/",
+  // base: process.env.VITE_BASE_PATH || "/gaddha", 
+}));
